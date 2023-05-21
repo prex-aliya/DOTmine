f62294ca22f219fc4f8426aae13bc3257b9d8d80
prex-aliya <impromptu_decency@proton.me>
2023-05-20 16:36:44 -1000

    add music script

---
+++ scripts/music.sh
@@ -0,0 +1,21 @@
+ME/Music
+
+findparms=(-name '*.mp3' -or -name '*.wav' -or -name '*.m4a')
+mpvparms=(--no-video)
+fzfparms=(--multi)
+
+declare -a plist
+
+if [ "$1" = "-m" ]; then
+	(find "$dir" "${findparms[@]}" | sed "s%${dir}/%%") \
+		| fzf "${fzfparms[@]}" | {
+
+		while IFS='' read -r f; do
+			plist+=("${dir}/${f}")
+		done
+
+		[[ ${#plist[@]} > 0 ]] && exec mpv "${mpvparms[@]}" "${plist[@]}"
+	}
+fi
+
+mpv --shuffle ${dir}
